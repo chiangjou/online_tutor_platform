9f7ca482fd48ccf7fc3842d14c127dc57573ddf6
chiangjou <tiffanyj.chiang@gmail.com>
2023-09-19 18:11:35 +0800

    feat: add flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const courseController = require('../controllers/course-controller')
 const userController = require('../controllers/user-controller')
 
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 const admin = require('./modules/admin')
 router.use('/admin', admin)
 
@@ -13,5 +15,6 @@ router.post('/signup', userController.signUp)
 router.get('/tutors', courseController.getTutors)
 
 router.use('/', (req, res) => res.redirect('/tutors'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
