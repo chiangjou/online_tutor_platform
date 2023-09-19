2007fb1f63107791ade59fce1e02d70e33e3dbc8
chiangjou <tiffanyj.chiang@gmail.com>
2023-09-19 18:06:48 +0800

    feat: add user signup page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const courseController = require('../controllers/course-controller')
+const userController = require('../controllers/user-controller')
 
 const admin = require('./modules/admin')
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 router.get('/tutors', courseController.getTutors)
 
