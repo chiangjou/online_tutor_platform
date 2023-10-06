ed96225180378137d216edec916a0e361e1a7941
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-10-06 21:09:40 +0800

    feat: add search bar on admin page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -96,6 +96,132 @@ const adminController = {
         })
       })
       .catch(err => next(err))
+  },
+  searchUsers: (req, res, next) => {
+    const keyword = req.query.keyword.trim()
+    const DEFAULT_LIMIT = 10
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return User.findAndCountAll({
+      raw: true,
+      nest: true,
+      attributes: { exclude: ['password'] },
+      where: { isAdmin: 0 },
+      limit,
+      offset
+    })
+      .then(users => {
+        const data = users.rows.map(user => ({
+          ...user,
+          name: user.name.toLowerCase(),
+          nation: user.nation.toLowerCase()
+        }))
+
+        const searchedUsers = data.filter(user => {
+          return (
+            user.name.includes(keyword) || user.nation.includes(keyword)
+          )
+        })
+
+        if (searchedUsers.length === 0) {
+          throw new Error(`沒有符合關鍵字「${keyword}」的用戶`)
+        }
+
+        return res.render('admin/users', {
+          data: searchedUsers,
+          pagination: getPagination(limit, page, users.count),
+          keyword
+        })
+      })
+      .catch(err => next(err))
+  },
+  searchStudents: (req, res, next) => {
+    const keyword = req.query.keyword.trim()
+    const DEFAULT_LIMIT = 10
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return User.findAndCountAll({
+      raw: true,
+      nest: true,
+      attributes: { exclude: ['password'] },
+      where: { isAdmin: 0, isTutor: 0 },
+      limit,
+      offset
+    })
+      .then(students => {
+        const data = students.rows.map(student => ({
+          ...student,
+          name: student.name.toLowerCase(),
+          nation: student.nation.toLowerCase()
+        }))
+
+        const searchedStudents = data.filter(student => {
+          return (
+            student.name.includes(keyword) || student.nation.includes(keyword)
+          )
+        })
+
+        if (searchedStudents.length === 0) {
+          throw new Error(`沒有符合關鍵字「${keyword}」的學生`)
+        }
+
+        return res.render('admin/students', {
+          data: searchedStudents,
+          pagination: getPagination(limit, page, students.count),
+          keyword
+        })
+      })
+      .catch(err => next(err))
+  },
+  searchTutors: (req, res, next) => {
+    const keyword = req.query.keyword.trim()
+    const DEFAULT_LIMIT = 10
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return User.findAndCountAll({
+      raw: true,
+      nest: true,
+      attributes: { exclude: ['password'] },
+      where: { isAdmin: 0, isTutor: 1 },
+      include: [{
+        model: Tutor,
+        attributes: ['tutorIntroduction', 'teachingStyle']
+      }],
+      limit,
+      offset
+    })
+      .then(tutors => {
+        const data = tutors.rows.map(tutor => ({
+          ...tutor,
+          name: tutor.name.toLowerCase(),
+          nation: tutor.nation.toLowerCase(),
+          tutorIntroduction: tutor.Tutor.tutorIntroduction.toLowerCase(),
+          teachingStyle: tutor.Tutor.teachingStyle.toLowerCase()
+        }))
+
+        const searchedTutors = data.filter(tutor => {
+          return (
+            tutor.name.includes(keyword) || tutor.nation.includes(keyword) || tutor.tutorIntroduction.includes(keyword) || tutor.teachingStyle.includes(keyword)
+          )
+        })
+
+        if (searchedTutors.length === 0) {
+          throw new Error(`沒有符合關鍵字「${keyword}」的老師`)
+        }
+
+        return res.render('admin/tutors', {
+          data: searchedTutors,
+          pagination: getPagination(limit, page, tutors.count),
+          keyword
+        })
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
 
+router.get('/users/search', authenticatedAdmin, adminController.searchUsers)
+router.get('/students/search', authenticatedAdmin, adminController.searchStudents)
+router.get('/tutors/search', authenticatedAdmin, adminController.searchTutors)
+
 router.get('/users', authenticatedAdmin, adminController.getUsers)
 router.get('/students', authenticatedAdmin, adminController.getStudents)
 router.get('/tutors', authenticatedAdmin, adminController.getTutors)
